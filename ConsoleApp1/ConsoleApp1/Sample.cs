486fa70dcfa87ca9b16c47f9327b120d8344e487
kapil93617 <51697236+kapil93617@users.noreply.github.com>
2019-06-19 12:49:05 +0530

    Add files via upload

--- ConsoleApp1/ConsoleApp1/Sample.cs
+++ ConsoleApp1/ConsoleApp1/Sample.cs
@@ -4,20 +4,61 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    class Sample
+    interface I1
     {
-        public void print()
+        void View();
+    }
+    interface I2
+    {
+        void View();
+    }
+    class Demo:I1
+    {
+        public void View()
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("Interfaec I1 Method");
+        }
+        public void View1()
+        {
+            Console.WriteLine("Demo class Method");
+        }
+        public void Display()
+        {
+            Console.WriteLine("Demo class hello Demo");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("Sample class hello");
         }
     }
+    class Sample: Demo
+    {
+        public override void Print()
+        {
+            Console.WriteLine(Sample class hello);
+        }
+    }
+
+    class Sample : Demo
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Sample class hello");
+        }
+    }
+
 
     class program
     {
         public static void Main(string [] args)
         {
-            Sample sap = new Sample();
-            sap.print();
+            Demo sap = new Sample();
+            sap.Print();
+            sap.Display();
+            sap.View1();
+            sap.View2();
         }
     }
+
+
 }
